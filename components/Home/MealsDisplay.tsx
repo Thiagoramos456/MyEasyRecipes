4bef44f6729a309a27fa17dd3e301a7f20b428f0
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-04-29 20:03:32 -0300

    Create meal page

--- components/Home/MealsDisplay.tsx
+++ components/Home/MealsDisplay.tsx
@@ -2,11 +2,11 @@ import { Box, Grid, Typography } from '@mui/material';
 import MealCard from '../MealCard';
 import IMeal from '../../types/IMeal';
 
-function MealsDisplay({ meals, categoryName } : IProps) {
+function MealsDisplay({ meals, disableTitle } : IProps) {
   return (
     <Box>
       <Grid my={4} display='flex' justifyContent='center' alignItems='center'>
-        <Typography variant='h5'>{ categoryName ?? "Meals" }</Typography>
+        <Typography variant='h5'>{ disableTitle && "Meals" }</Typography>
       </Grid>
 			<Box display='flex' gap={2} flexWrap='wrap' justifyContent='center' alignItems='center'>
 			{ meals?.map((meal) => <MealCard meal={meal} /> ) }
@@ -17,7 +17,7 @@ function MealsDisplay({ meals, categoryName } : IProps) {
 
 interface IProps {
   meals: IMeal[];
-  categoryName: string;
+  disableTitle?: boolean;
 }
 
 export default MealsDisplay;
--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -1,35 +1,66 @@
 import { useRouter } from 'next/router';
-import { getAllCategories, getMealsByCategory } from '../../services/MealApiService';
+import {
+  getAllCategories,
+  getMealsByCategory,
+} from '../../services/MealApiService';
 import IMeal from '../../types/IMeal';
 import MealsDisplay from '../../components/Home/MealsDisplay';
+import ICategory from '../../types/ICategory';
+import { Box, Grid, Paper, Typography } from '@mui/material';
 
-function CategoryPage({ meals, categoryName } : IProps) {
-	return <>
-		<MealsDisplay meals={ meals } categoryName={categoryName} />
-	</>
+function CategoryPage({ meals, category }: IProps) {
+  return (
+    <>
+      <Grid my={4} display='flex' justifyContent='center' alignItems='center'>
+        <Typography variant='h5'>{category.strCategory}</Typography>
+      </Grid>
+      <Box my={3} display={'flex'} justifyContent='center'>
+        <Box
+          component='img'
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+          }}
+          alt={category.strCategory}
+          src={category.strCategoryThumb}
+        />
+      </Box>
+      <Typography>{category.strCategoryDescription}</Typography>
+      <MealsDisplay meals={meals} />
+    </>
+  );
 }
 
 interface IProps {
   meals: IMeal[];
-	categoryName: string;
+  category: ICategory;
 }
 
 export async function getStaticPaths() {
-	const categories = await getAllCategories();
-
-	const categoriesParams = categories.map(c => ({ params: { category: c.strCategory } }))
+  const categories = await getAllCategories();
+  const categoriesParams = categories.map((c) => ({
+    params: { category: c.strCategory },
+  }));
 
   return {
     paths: categoriesParams,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
-	console.log(params)
-	const meals = await getMealsByCategory(params.category)
+  const meals = await getMealsByCategory(params.category);
+  const categories = await getAllCategories();
+  const category = categories.find((c) => c.strCategory === params.category);
 
-  return { props: { meals, categoryName: params.category } }
+  return {
+    props: {
+      meals,
+      category,
+    },
+  };
 }
 
 export default CategoryPage;
