bb2a457320fb94c7aea17f3a4770d5a48fc38245
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-04-29 22:08:32 -0300

    Remove disable meals flag

--- components/Home/MealsDisplay.tsx
+++ components/Home/MealsDisplay.tsx
@@ -2,11 +2,11 @@ import { Box, Grid, Typography } from '@mui/material';
 import MealCard from '../MealCard';
 import IMeal from '../../types/IMeal';
 
-function MealsDisplay({ meals, disableTitle } : IProps) {
+function MealsDisplay({ meals } : IProps) {
   return (
     <Box>
       <Grid my={4} display='flex' justifyContent='center' alignItems='center'>
-        <Typography variant='h5'>{ disableTitle && "Meals" }</Typography>
+        <Typography variant='h5'>{ "Meals" }</Typography>
       </Grid>
 			<Box display='flex' gap={2} flexWrap='wrap' justifyContent='center' alignItems='center'>
 			{ meals?.map((meal) => <MealCard meal={meal} /> ) }
