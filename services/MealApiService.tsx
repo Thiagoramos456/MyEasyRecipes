c9f8b2785cf2b2898a7d72e6e32c4aa2e8ab3311
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-04-30 11:12:16 -0300

    Commit test 4

--- services/MealApiService.tsx
+++ services/MealApiService.tsx
@@ -4,13 +4,11 @@ import IMeal from '../types/IMeal';
 export async function getAllCategories(): Promise<ICategory[]> {
 	const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
   const data = await res.json();
-
 	return data.categories as ICategory[];
 }
 
 export async function getMealsByCategory(category: string): Promise<IMeal[]> {
 	const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
   const data = await res.json();
-	
 	return data.meals as IMeal[];
 }
