e8d7d097deab5cbb54fe9c58da7dc64d0de4f128
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-04-30 22:33:59 -0300

    Fix IMeal importer

--- components/Home/MealsDisplay.tsx
+++ components/Home/MealsDisplay.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Typography } from '@mui/material';
 import MealCard from '../MealCard';
-import IMeal from '../../types/IMeal';
+import { IMeal } from '../../types/IMeal';
 
 function MealsDisplay({ meals } : IProps) {
   return (
