99e915b8dc42ba1195492b809ce03fa55cb3a3d4
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-05-01 09:38:50 -0300

    Add loading to meal card

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -4,14 +4,20 @@ import {
   CardContent,
   Typography,
   CardActionArea,
+  CircularProgress,
+  Box,
 } from '@mui/material';
 import Link from 'next/link';
 import { IMeal } from '../types/IMeal';
+import { orange } from '@mui/material/colors';
+import { useState } from 'react';
 
 function MealCard({ meal }: IProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <Card sx={{ width: 200, height: 230, px: 1 }}>
-      <CardActionArea>
+    <Card sx={{ width: 200, height: 230, px: 1, py: 1, position: 'relative' }}>
+      <CardActionArea onClick={() => setIsLoading(true)} sx={{}}>
         <Link href={`/meals/${meal.idMeal}`}>
           <CardMedia
             sx={{ height: 140 }}
@@ -36,12 +42,40 @@ function MealCard({ meal }: IProps) {
           </CardContent>
         </Link>
       </CardActionArea>
+      {isLoading && (
+        <>
+          <CircularProgress
+            size={40}
+            sx={{
+              color: orange[300],
+              position: 'absolute',
+              top: 80,
+              left: 89,
+              zIndex: 2,
+            }}
+          />
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              bgcolor: 'rgba(0, 0, 0, 0.40)',
+              color: 'white',
+              padding: '10px',
+              zIndex: 1,
+            }}
+          />
+        </>
+      )}
     </Card>
   );
 }
 
 interface IProps {
   meal: IMeal;
+  isLoading: boolean;
 }
 
 export default MealCard;
