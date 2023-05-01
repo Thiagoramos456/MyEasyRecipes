807a913f4fbb18100d60971143f3d38a0e763f45
Thiagoramos456 <thiagovirtual10@gmail.com>
2023-05-01 08:45:35 -0300

    Fix meal card to be like category card hover

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -1,33 +1,41 @@
-import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActionArea,
+} from '@mui/material';
 import Link from 'next/link';
 import { IMeal } from '../types/IMeal';
 
 function MealCard({ meal }: IProps) {
   return (
     <Card sx={{ width: 200, height: 230, px: 1 }}>
-      <Link href={`/meals/${meal.idMeal}`}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={meal.strMealThumb}
-          title={meal.strMeal}
-        />
-        <CardContent>
-          <Typography
-            sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              display: '-webkit-box',
-              WebkitLineClamp: '2',
-              WebkitBoxOrient: 'vertical',
-            }}
-            gutterBottom
-            variant='h6'
-            component='div'
-          >
-            {meal.strMeal}
-          </Typography>
-        </CardContent>
-      </Link>
+      <CardActionArea>
+        <Link href={`/meals/${meal.idMeal}`}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={meal.strMealThumb}
+            title={meal.strMeal}
+          />
+          <CardContent>
+            <Typography
+              sx={{
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                display: '-webkit-box',
+                WebkitLineClamp: '2',
+                WebkitBoxOrient: 'vertical',
+              }}
+              gutterBottom
+              variant='h6'
+              component='div'
+            >
+              {meal.strMeal}
+            </Typography>
+          </CardContent>
+        </Link>
+      </CardActionArea>
     </Card>
   );
 }
